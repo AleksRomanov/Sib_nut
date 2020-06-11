9b7f4ae5d456d6f857a2bd1f771c56f17a2a99da
AleksRomanov <kosmonavt-vavilon@mail.ru>
2020-06-11 15:53:12 +0700

    Адаптив
    Swiper About

---
+++ themes/nuts/assets/js/swiper_about.js
@@ -0,0 +1,12 @@
+const swiperAbout = () => {
+    new Swiper('.about__container', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    });
+};
+
+export default swiperAbout;
