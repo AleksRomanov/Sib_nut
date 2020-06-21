2a65c51eb5c8923ecdd4367a8a5118cb8a8a3957
AleksRomanov <kosmonavt-vavilon@mail.ru>
2020-06-21 17:36:41 +0700

    fotorama. rework
    adaptive all sections
    
    доработать background галереи на mobile
    доработать map
    доработать partners на mobile(шрифт)
    
    начало products

--- themes/nuts/assets/js/swiper_products.js
+++ themes/nuts/assets/js/swiper_products.js
@@ -1,7 +1,8 @@
 const swiperProducts = () => {
     new Swiper('.products__swiper', {
-        slidesPerView: 1,
-        spaceBetween: 30,
+        slidesPerView: 3,
+        spaceBetween: 0,
+        direction: 'horizontal',
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
